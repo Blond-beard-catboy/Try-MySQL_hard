bbfb3d4ea699fb0bcb282a766bf7d4db1fa951e5
Саша <paradoxlover13@gmail.com>
2023-01-03 18:32:46 +0500

    123

---
+++ config.py
@@ -0,0 +1,4 @@
+host = "172.20.65.243"
+user = "user1"
+password = "123456"
+db_name = "testdatabase"
---
+++ main.py
@@ -0,0 +1,27 @@
+import pymysql
+from config import host, user, password, db_name
+
+try:
+    connection = pymysql.connect(
+        host = host,
+        port = 3306,
+        user = user,
+        password = password,
+        database = db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("successfully connected...")
+    print("#" * 20)
+
+    try:
+        with connection.cursor() as cursor:
+            sql = ""
+
+            cursor.execute(sql)
+            connection.commit()
+    finally:
+        connection.close()
+
+except Exception as ex:
+    print("Connection refused...")
+    print(ex)
