af6544f208759272e58be5c02816611ccd03f063
Blond-beard-catboy <88153641+Blond-beard-catboy@users.noreply.github.com>
2023-01-05 13:26:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import pymysql
 from config import host, user, password, db_name
 
+
 try:
     connection = pymysql.connect(
-        host = host,
-        port = 3306,
-        user = user,
-        password = password,
-        database = db_name,
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
         cursorclass=pymysql.cursors.DictCursor
     )
-    print("succesfully connected...")
+    print("successfully connected...")
     print("#" * 20)
+    try:
+       #with connection.cursor() as cursor:
+            #create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT, name varchar(32), password varchar(32), email varchar(32), PRIMARY KEY(id));"
+            #cursor.execute(create_table_query)
+            #print("Table created successfully")
+        #insert data
+        #with connection.cursor() as cursor:
+            #insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Anna', 'qwerty', 'anna@gmail.com');"
+            #cursor.execute(insert_query)
+            #connection.commit()
+        with connection.cursor() as cursor:
+            select_all_rows = "SELECT * FROM `users`"
+            cursor.execute(select_all_rows)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+    finally:
+        connection.close()
 
 except Exception as ex:
     print("Connection refused...")
